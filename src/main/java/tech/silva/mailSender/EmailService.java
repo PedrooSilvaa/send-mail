c04471ae1b0a52632208ea23dafb37526e769b7a
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-30 10:52:32 -0300

    feat: create EmailService and send Method

---
+++ src/main/java/tech/silva/mailSender/EmailService.java
@@ -0,0 +1,33 @@
+package tech.silva.mailSender;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public String sendMail(String recipient, String subject, String message) {
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom(sender);
+            simpleMailMessage.setTo(recipient);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(message);
+            javaMailSender.send(simpleMailMessage);
+            return "Mail send sucessfully";
+        } catch (Exception ex) {
+            return "New exception: " + ex.getLocalizedMessage();
+        }
+    }
+}
